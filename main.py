dd8b392fe46a9835fb77562a48a2f712499ece47
Frank Assumma <fassumma@calpoly.edu>
2024-03-13 13:29:02 -0700

    updates to main

--- main.py
+++ main.py
@@ -29,10 +29,10 @@ def read_shadow_file(file_path):
                 shadow_data.append((username, algorithm, int(workfactor), salt, hashed_password, algorithm_workfactor_salt_hash))
     return shadow_data
 
-# Generate potential passwords
 def generate_passwords():
     nltk.download('words')
     word_corpus = nltk.corpus.words.words()
+    #print(word_corpus)
     potential_passwords = []
     for word in word_corpus:
         if 6 <= len(word) <= 10:
@@ -42,19 +42,29 @@ def generate_passwords():
 def crack_passwords(shadow_data, potential_passwords):
     cracked_passwords = {}
     start_time = time.time()
+    curr_ascii = 96
+    #print(curr_ascii)
     #print(shadow_data)
     for username, algorithm, workfactor, salt, hashed_password, alg_wf_sh in shadow_data:
         for password in potential_passwords:
-            # Hash password with given salt and work factor
+            ascii = ord(password[0])
+            #print(ascii)
+            #print(ascii)
+            if(ascii != curr_ascii and abs(ascii-curr_ascii) != 32):
+                curr_ascii = ascii
+                print("new char: ", ord(password[0]), "\n")
+            #print(password)
+            # Hash password with given salt and work factor???
             # print(bcrypt.gensalt(workfactor, algorithm.encode()))
             updated_salt = f"${algorithm}${workfactor}${salt}"
             # hashed_attempt = bcrypt.hashpw(password.encode(), bcrypt.gensalt(workfactor, prefix=algorithm.encode()))
             #print(updated_salt)
             #print(bcrypt.gensalt())
             hashed_attempt = bcrypt.hashpw(password.encode(), updated_salt.encode())
-            # Compare hashed attempt with hashed password from shadow file
-            print(bcrypt.checkpw(hashed_attempt, alg_wf_sh.encode()))
-            if bcrypt.checkpw(hashed_attempt, alg_wf_sh.encode()):
+            #print(hashed_attempt, alg_wf_sh.encode())
+            # Compare hashed attempt with hashed password from shadow file??
+            #print(bcrypt.checkpw(hashed_attempt, alg_wf_sh.encode()))
+            if hashed_attempt == alg_wf_sh.encode():
                 print(f"found a pass: {password}!")
                 cracked_passwords[username] = password
                 break
@@ -62,13 +72,18 @@ def crack_passwords(shadow_data, potential_passwords):
     time_taken = end_time - start_time
     return cracked_passwords, time_taken
 
-
-# Main function
 def main():
     shadow_file_path = 'shadow.txt'
     shadow_data = read_shadow_file(shadow_file_path)
+    # print(shadow_data)
     potential_passwords = generate_passwords()
+    # passwords = ["dog", "cat"]
+    # dog = "aardvark"
+    # salt = "$2b$08$J9FW66ZdPI2nrIMcOxFYI."
+    # hash_dog = bcrypt.hashpw(dog.encode(), salt.encode())
+    # print(hash_dog)
     #print(potential_passwords)
+    #cracked_passwords, time_taken = crack_passwords(shadow_data, potential_passwords)
     cracked_passwords, time_taken = crack_passwords(shadow_data, potential_passwords)
     print("Cracked passwords:")
     for username, password in cracked_passwords.items():
